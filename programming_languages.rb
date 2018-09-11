bc25caec351f5d796a1c9328ba2c2050067b3a60
Rob Parker <into.the.parker@gmail.com>
2018-09-11 13:06:06 +0000

    automatically backed up by learn

---
+++ programming_languages.rb
@@ -0,0 +1,12 @@
+def reformat_languages(languages)
+    new_hash = {}
+    languages.each do |key, value|
+      value.each do |i,k|
+        new_hash[i] = k
+        new_hash[i][:style] = [key]
+      end
+    end
+
+   new_hash[:javascript][:style] << [:oo]
+   new_hash
+  end  
